25a6e08ef63d428eb5189ea73de40e5d1da92303
Express Lingoport <familysearch@lingoport.com>
2016-02-08 16:27:27 -0700

    Added a "This is new" concat string.

--- src/com/lingoport/demo/java/ExampleMain.java
+++ src/com/lingoport/demo/java/ExampleMain.java
@@ -52,6 +52,7 @@ public class ExampleMain {
         frame.pack();
         frame.setVisible(true);
         String strTitle = "Example Address Book -- " + locale;
+        String addedString = " This is new " + locale;
         frame.setTitle(strTitle);
     }
 
